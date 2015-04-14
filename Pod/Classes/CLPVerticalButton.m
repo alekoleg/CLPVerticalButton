d418cad053e1fb2149cff50f1247d396df26f9dd
Alekseenko Oleg <AlekOleg@iMac-AlekOleg-2.local>
2015-04-14 14:27:10 +0300

    - vertical aligment

--- Pod/Classes/CLPVerticalButton.m
+++ Pod/Classes/CLPVerticalButton.m
@@ -47,6 +47,7 @@
 }
 
 
+
 - (void)layoutSubviews {
     [super layoutSubviews];
     
@@ -65,18 +66,24 @@
         if (image) {
             image = [self imageForState:UIControlStateNormal];
         }
-       
+        
         NSStringDrawingOptions options = (self.multiLines) ? NSStringDrawingUsesLineFragmentOrigin : NSStringDrawingTruncatesLastVisibleLine;
         CGSize textSize = [string boundingRectWithSize:CGSizeMake(self.frame.size.width, MAXFLOAT) options:options attributes:@{ NSFontAttributeName : self.titleLabel.font} context:NULL].size;
         CGFloat offset = (self.frame.size.height - image.size.height - self.verticalMargin - textSize.height) / 2;
+        
+        if ([self isVerticalTop]) {
+            offset = 0;
+        } else if ([self isVerticalBottom]) {
+            offset = offset * 2;
+        }
         
         self.imageView.frame = ({
             CGRect frame = self.imageView.frame;
             frame.origin.y = offset;
             frame.size = image.size;
-            if (self.contentMode == UIViewContentModeLeft) {
+            if ([self isHorizontalLeft]) {
                 frame.origin.x = 0;
-            } else if (self.contentMode == UIViewContentModeRight){
+            } else if ([self isHorizontalRigth]){
                 frame.origin.x = self.frame.size.width - frame.size.width;
             } else {
                 frame.origin.x = (self.frame.size.width - frame.size.width) / 2;
@@ -87,9 +94,9 @@
         self.titleLabel.frame = ({
             CGRect frame = self.titleLabel.frame;
             frame.size = textSize;
-            if (self.contentMode == UIViewContentModeLeft) {
+            if ([self isHorizontalLeft]) {
                 frame.origin.x = 0;
-            } else if (self.contentMode == UIViewContentModeRight){
+            } else if ([self isHorizontalRigth]){
                 frame.origin.x = self.frame.size.width - frame.size.width;
             } else {
                 frame.origin.x = (self.frame.size.width - frame.size.width) / 2;
@@ -98,6 +105,30 @@
             frame;
         });
     }
+}
+
+- (BOOL)isHorizontalLeft {
+    return (self.contentMode == UIViewContentModeLeft || self.contentMode == UIViewContentModeTopLeft || self.contentMode == UIViewContentModeBottomLeft);
+}
+
+- (BOOL)isHorizontalCenter {
+    return (self.contentMode == UIViewContentModeCenter || self.contentMode == UIViewContentModeTop || self.contentMode == UIViewContentModeBottom);
+}
+
+- (BOOL)isHorizontalRigth {
+    return (self.contentMode == UIViewContentModeRight || self.contentMode == UIViewContentModeBottomRight || self.contentMode == UIViewContentModeTopRight);
+}
+
+- (BOOL)isVerticalTop {
+    return (self.contentMode == UIViewContentModeTop || self.contentMode == UIViewContentModeTopLeft || self.contentMode == UIViewContentModeRight);
+}
+
+- (BOOL)isVerticalCenter {
+    return (self.contentMode == UIViewContentModeCenter || self.contentMode == UIViewContentModeTopLeft || self.contentMode == UIViewContentModeRight);
+}
+
+- (BOOL)isVerticalBottom {
+    return (self.contentMode == UIViewContentModeBottom || self.contentMode == UIViewContentModeBottomLeft || self.contentMode == UIViewContentModeBottomRight);
 }
 
 @end
